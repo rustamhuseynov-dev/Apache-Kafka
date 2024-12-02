4a263595a631dcb7db5b459895350aeb903355c4
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-02 21:42:28 +0400

    Create UserDto.java

---
+++ kafka-consumer/src/main/java/com/rustam/kafka_consumer/UserDto.java
@@ -0,0 +1,18 @@
+package com.rustam.kafka_consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private Integer age;
+}

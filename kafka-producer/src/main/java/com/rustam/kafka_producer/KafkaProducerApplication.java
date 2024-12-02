60684871251f397a9bbee9c0d247bf5739cf126f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-02 16:37:55 +0400

    Update KafkaProducerApplication.java

--- kafka-producer/src/main/java/com/rustam/kafka_producer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/rustam/kafka_producer/KafkaProducerApplication.java
@@ -2,8 +2,10 @@ package com.rustam.kafka_producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class KafkaProducerApplication {
 
 	public static void main(String[] args) {

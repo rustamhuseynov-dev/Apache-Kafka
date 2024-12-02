6b25a5561d4a08cf485e98e006f70657b28a5db3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-02 16:37:37 +0400

    Create MessageService.java

---
+++ kafka-consumer/src/main/java/com/rustam/kafka_consumer/MessageService.java
@@ -0,0 +1,12 @@
+package com.rustam.kafka_consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class MessageService {
+
+
+}

95d709ca6ef6f25b91fcdaa47fb4af0321206832
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-02 21:42:43 +0400

    Update KafkaController.java

--- kafka-producer/src/main/java/com/rustam/kafka_producer/controller/KafkaController.java
+++ kafka-producer/src/main/java/com/rustam/kafka_producer/controller/KafkaController.java
@@ -1,10 +1,13 @@
 package com.rustam.kafka_producer.controller;
 
+import com.rustam.kafka_producer.dto.UserDto;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,11 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class KafkaController {
 
-    KafkaTemplate<String,String> kafkaTemplate;
+
+    KafkaTemplate<String,Object> kafkaTemplate;
 
     @PostMapping(path = "/send")
     public void sendMessage(@RequestParam String message){
-        kafkaTemplate.send("consumer",message);
-        log.info("send message {}",message);
+        var user = UserDto.builder()
+                .id(1L)
+                .age(15)
+                .name("Rustam")
+                .surname("Huseynov")
+                .build();
+        var pr = new ProducerRecord<String,Object>("consumer",user);
+        kafkaTemplate.send(pr);
+        log.info("send message key {}",pr.key());
+        log.info("send message valur {}",pr.value());
     }
 }

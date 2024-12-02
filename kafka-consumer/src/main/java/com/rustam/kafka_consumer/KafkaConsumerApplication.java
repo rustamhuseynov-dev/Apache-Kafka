29b9a254b2ab0ad126ec8262507cb291c53b083c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-02 16:37:33 +0400

    Update KafkaConsumerApplication.java

--- kafka-consumer/src/main/java/com/rustam/kafka_consumer/KafkaConsumerApplication.java
+++ kafka-consumer/src/main/java/com/rustam/kafka_consumer/KafkaConsumerApplication.java
@@ -1,13 +1,23 @@
 package com.rustam.kafka_consumer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@Slf4j
+@EnableKafka
 public class KafkaConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
 
+	@KafkaListener(topics = "consumer")
+	public void listener(String data){
+		log.info("message received {}",data);
+	}
 }

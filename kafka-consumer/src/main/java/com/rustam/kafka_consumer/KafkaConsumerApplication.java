9dbf6e3b15a35c456d31fe63e44c5b1fc2524094
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-02 21:42:18 +0400

    Update KafkaConsumerApplication.java

--- kafka-consumer/src/main/java/com/rustam/kafka_consumer/KafkaConsumerApplication.java
+++ kafka-consumer/src/main/java/com/rustam/kafka_consumer/KafkaConsumerApplication.java
@@ -1,6 +1,10 @@
 package com.rustam.kafka_consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,14 +14,18 @@ import org.springframework.kafka.annotation.KafkaListener;
 @SpringBootApplication
 @Slf4j
 @EnableKafka
+@RequiredArgsConstructor
 public class KafkaConsumerApplication {
+
+	private final ObjectMapper objectMapper;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
 
-	@KafkaListener(topics = "consumer")
-	public void listener(String data){
+	@KafkaListener(topics = "consumer",containerFactory = "customKafkaListenerContainerFactory")
+	public void listener(ConsumerRecord<String,String> data) throws JsonProcessingException {
+		UserDto userDto = objectMapper.readValue(data.value(), UserDto.class);
 		log.info("message received {}",data);
 	}
 }

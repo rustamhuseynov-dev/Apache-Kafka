d7070c964be1d13648ad4355100729602fd4cbb4
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-03 12:03:24 +0400

    Create User.java

---
+++ kafka-consumer/src/main/java/com/rustam/kafka_consumer/User.java
@@ -0,0 +1,23 @@
+package com.rustam.kafka_consumer;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "kafka_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private Integer age;
+}
